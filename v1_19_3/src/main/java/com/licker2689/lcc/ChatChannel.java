9cebef726de7218fef3c5b790a668dcd77c95b00
licker2689 <83544855+licker2689@users.noreply.github.com>
2023-04-14 01:05:03 +0900

    1.19.3

---
+++ v1_19_3/src/main/java/com/licker2689/lcc/ChatChannel.java
@@ -0,0 +1,32 @@
+package com.licker2689.lcc;
+
+ 
+import com.licker2689.lpc.utils.DataContainer;
+import com.licker2689.lcc.commands.LCCCommand;
+import com.licker2689.lcc.events.LCCEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChatChannel extends JavaPlugin {
+    private static ChatChannel plugin;
+    public static DataContainer data;
+    public static boolean force;
+
+    public static ChatChannel getInstance() {
+        return plugin;
+    }
+
+    public void onEnable() {
+        plugin = this;
+        data = new DataContainer(plugin);
+        force = data.getConfig().getBoolean("Settings.forceUseChannel");
+        plugin.getServer().getPluginManager().registerEvents(new LCCEvent(), plugin);
+        getCommand("lcc").setExecutor(new LCCCommand());
+
+    }
+
+    public void onDisable() {
+        Bukkit.getOnlinePlayers().forEach(player -> data.saveAndLeave(player.getUniqueId()));
+        data.save();
+    }
+}
---
+++ v1_19_3/src/main/java/com/licker2689/lcc/commands/LCCCommand.java
@@ -0,0 +1,70 @@
+package com.licker2689.lcc.commands;
+
+import com.licker2689.lpc.utils.DataContainer;
+import com.licker2689.lcc.ChatChannel;
+import com.licker2689.lcc.functions.LCCFunction;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LCCCommand implements CommandExecutor, TabCompleter {
+    private final DataContainer data = ChatChannel.data;
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        if(!(sender instanceof Player)) {
+            sender.sendMessage(data.getPrefix() + "플레이어만 사용 가능합니다.");
+            return false;
+        }
+        Player p = (Player) sender;
+        if(args.length == 0) {
+            if(p.isOp()) {
+                p.sendMessage(data.getPrefix() + "/lcc force : 채팅 채널 강제사용 설정 (토글)");
+            }
+            p.sendMessage(data.getPrefix() + "/lcc 선택 <채널번호> - 해당 채널로 채팅 채널이 변경됩니다.");
+            p.sendMessage(data.getPrefix() + "/lcc 전체 - 전체 채팅으로 전환 합니다.");
+            return false;
+        }
+        if(args[0].equalsIgnoreCase("force")) {
+            if(!p.isOp()) {
+                p.sendMessage(data.getPrefix() + "권한이 없습니다.");
+                return false;
+            }
+            LCCFunction.force(p);
+            return false;
+        }
+        if(args[0].equalsIgnoreCase("전체")) {
+            LCCFunction.setDefault(p);
+            return false;
+        }
+        if(args[0].equalsIgnoreCase("선택")) {
+            if(args.length == 1) {
+                p.sendMessage(data.getPrefix() + "/lcc 선택 <채널번호> - 해당 채널로 채팅 채널이 변경됩니다.");
+                return false;
+            }
+            LCCFunction.setChannel(p, args[1]);
+            return false;
+        }
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        if(args.length == 1) {
+            if(sender.isOp()) {
+                return Arrays.asList("전체", "선택", "force");
+            }else{
+                return Arrays.asList("전체", "선택");
+            }
+        }
+        return null;
+    }
+}
